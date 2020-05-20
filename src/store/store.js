9c221bdf374e8e961955491ba71e19f7a13dfdac
Dominic Silvia <dominic.silvia@gmail.com>
2020-05-19 19:16:12 -0700

    Complete planning state

--- src/store/store.js
+++ src/store/store.js
@@ -4,3 +4,10 @@ import rootReducer from './../reducers/main.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() (after reducer in createStore(reduce, ...))
+
+/* Application State
+videos
+currentVideo
+*/
+
