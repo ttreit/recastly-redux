cd976cfd731a833928e3ce5ac8630741d3c76cd6
ttreit <14933277+ttreit@users.noreply.github.com>
2020-05-23 11:06:43 -0700

    Add redux dev tools

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,15 @@ import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
-var store = createStore(rootReducer, applyMiddleware(thunk));
+var composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+var store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
+
+//DEVTOOLS\\
+
+//to switch back to no devtools (so store test passes) Comment out the two lines below and uncomment line 20
+//var store = createStore(rootReducer, applyMiddleware(thunk));
+
+
+
